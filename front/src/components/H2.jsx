af2ab5493ad32214f39dad17746c0e371510fe3f
Gabriel Mendes <gabriel.26056@gmail.com>
2023-07-29 23:27:06 -0300

    fixed component h2 tag

--- front/src/components/H2.jsx
+++ front/src/components/H2.jsx
@@ -1,7 +1,7 @@
 export default function H2({ title }) {
   return (
     <div>
-      <h1 className="text-base mt-5 mb-8">{title}</h1>
+      <h2 className="text-base mt-5 mb-8">{title}</h2>
       <hr className="border-[#C6C6C6]" />
     </div>
   )
