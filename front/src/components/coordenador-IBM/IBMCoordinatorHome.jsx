17470fbd82e49e260367e7f800eb5287661d37dd
Gabriel Mendes <gabriel.26056@gmail.com>
2023-07-29 23:27:31 -0300

    add page content

--- front/src/components/coordenador-IBM/IBMCoordinatorHome.jsx
+++ front/src/components/coordenador-IBM/IBMCoordinatorHome.jsx
@@ -3,6 +3,10 @@
 import UserInfo from '../UserInfo'
 import { useContext } from 'react'
 import { PageContext } from '@/app/context/PageContext'
+import H2 from '../H2'
+import Card from '../Card'
+import Dropdown from '../Dropdown'
+import Task from '../Task'
 
 export default function IBMCoordinatorHome() {
   const { setUserType } = useContext(PageContext)
@@ -10,7 +14,7 @@ export default function IBMCoordinatorHome() {
   return (
     <main className="px-4 my-16">
       {/* Coordinator info */}
-      <UserInfo name="Guilherme" status="CoordenadorIBM" />
+      <UserInfo name="Guilherme" status="Coordenador IBM" />
 
       <p
         className="underline absolute right-0 top-2 h-10 z-50 text-xxs"
@@ -20,6 +24,40 @@ export default function IBMCoordinatorHome() {
       >
         Trocar usuário
       </p>
+
+      {/* Schools */}
+      {/* <H2 title="Escolas" /> */}
+      <h2 className="text-base mt-5 mb-8">Escolas</h2>
+
+      <Dropdown title="ETEC Zona Leste">
+        <Card>
+          <span className="text-base">1º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+        <Card>
+          <span className="text-base">2º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+        <Card>
+          <span className="text-base">3º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+      </Dropdown>
+
+      <Dropdown title="ETEC Americana">
+        <Card>
+          <span className="text-base">1º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+        <Card>
+          <span className="text-base">2º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+        <Card>
+          <span className="text-base">3º AMS - FATEC</span>
+          <span>2023</span>
+        </Card>
+      </Dropdown>
     </main>
   )
 }
