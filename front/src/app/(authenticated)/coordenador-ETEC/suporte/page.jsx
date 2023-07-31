901b5f32ce9dc34cba9078c68376e7547f804646
Gabriel Mendes <gabriel.26056@gmail.com>
2023-07-30 22:15:04 -0300

    complaint is now support

---
+++ front/src/app/(authenticated)/coordenador-ETEC/suporte/page.jsx
@@ -0,0 +1,10 @@
+import H1 from '@/components/H1'
+import Main from '@/components/Main'
+
+export default function Suporte() {
+  return (
+    <Main>
+      <H1 title="Solicitações de suporte" />
+    </Main>
+  )
+}
