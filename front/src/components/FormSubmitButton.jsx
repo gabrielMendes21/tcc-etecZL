23baa855928aa5fc715fc51608337ccfe3295c88
Gabriel Mendes <gabriel.26056@gmail.com>
2023-08-01 09:36:20 -0300

    change FormButton component name

--- front/src/components/FormSubmitButton.jsx
+++ front/src/components/FormSubmitButton.jsx
@@ -1,6 +1,6 @@
 import { ArrowRight } from 'lucide-react'
 
-export default function FormButton({ title }) {
+export default function FormSubmitButton({ title }) {
   return (
     <button className="flex justify-between items-center w-full text-left font-light bg-highlighted hover:brightness-110 transition-all text-white mt-7 p-3">
       {title}
