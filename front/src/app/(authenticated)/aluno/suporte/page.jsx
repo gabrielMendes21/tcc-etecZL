9c19b033836fa6c760dd144f7458509ba3098c42
Gabriel Mendes <gabriel.26056@gmail.com>
2023-08-01 09:30:09 -0300

    add new request button

--- front/src/app/(authenticated)/aluno/suporte/page.jsx
+++ front/src/app/(authenticated)/aluno/suporte/page.jsx
@@ -4,6 +4,7 @@ import Card from '@/components/Card'
 import Dropdown from '@/components/Dropdown'
 import PageTitle from '@/components/H1'
 import Main from '@/components/Main'
+import NewButton from '@/components/NewButton'
 
 export default function Suporte() {
   return (
@@ -33,28 +34,9 @@ export default function Suporte() {
           <span>Respondida em 12/05/2023</span>
         </Card>
       </Dropdown>
-      {/* <div
-          className="dropdown flex items-end gap-2 my-6"
-          onClick={handleDropDown}
-        >
-          <h3 className="font-medium">Respondidas</h3>
-          {isActive ? <ChevronDown size={20} /> : <ChevronRight size={20} />}
-        </div>
-        <div
-          style={{
-            display: isActive ? 'block' : 'none',
-          }}
-          className="activities space-y-5"
-        >
-          <Card>
-            <span className="text-base">Atraso nas atividades</span>
-            <span>Respondida em 20/09/2023</span>
-          </Card>
-          <Card>
-            <span className="text-base">Dúvidas sobre a atividade</span>
-            <span>Respondida em 12/05/2023</span>
-          </Card>
-        </div> */}
+
+      {/* New support requeat button */}
+      <NewButton text="Nova solicitação" to="suporte/nova-solicitacao" />
     </Main>
   )
 }
