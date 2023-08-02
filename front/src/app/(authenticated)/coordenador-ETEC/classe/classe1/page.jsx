e5aa6ad89b7f7bcdf710f51ffd4dbff04cb52bb3
Gabriel Mendes <gabriel.26056@gmail.com>
2023-08-02 14:33:46 -0300

    add support and classes routes

---
+++ front/src/app/(authenticated)/coordenador-ETEC/classe/classe1/page.jsx
@@ -0,0 +1,10 @@
+import H1 from '@/components/H1'
+import Main from '@/components/Main'
+
+export default function Classe() {
+  return (
+    <Main>
+      <H1 title="1º AMS" />
+    </Main>
+  )
+}
