ef63bad54cb72ab5087f7f3b998b261d5d0e78da
Gabriel Mendes <gabriel.26056@gmail.com>
2023-08-16 22:30:20 -0300

    adding sent activities

--- front/src/app/(authenticated)/coordenador-IBM/atividades/pitch/page.jsx
+++ front/src/app/(authenticated)/coordenador-IBM/atividades/pitch/page.jsx
@@ -1,52 +1,53 @@
-import Delay from '@/components/LoadDelay'
+import H2 from '@/components/H2'
+import Main from '@/components/Main'
 import { ArrowRight, Image } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Pitch() {
   return (
-    <main className="px-4 my-16">
-      <Delay>
-        {/* Task info */}
-        <div className=" space-y-10">
-          <div className="pt-7">
-            <h1 className="text-center font-medium text-xl">
-              Pitch de apresentação
-            </h1>
-            <span className="block text-center">19/05/2023</span>
-          </div>
-
-          <hr className="border-[#C6C6C6]" />
-
-          {/* Task details */}
-          <div>
-            <h2 className="font-medium">Detalhes da tarefa:</h2>
-            <p className="p-3 text-justify text-xs leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              iure corporis hic sit voluptate odit consequatur, eaque eum
-              voluptatum, quaerat dolorum veritatis quis exercitationem error
-              nesciunt quibusdam repudiandae. Assumenda, placeat!
-            </p>
-          </div>
-
-          {/* Attached files */}
-          <div>
-            <h2 className="font-medium mb-4">Materiais disponíveis:</h2>
-            <div className="border-2 border-highlighted text-highlighted p-3 flex gap-2 underline">
-              <Image alt="image icon" />
-              Arquivo1.png
-            </div>
-          </div>
-
-          {/* Edit task link */}
-          <Link
-            href="pitch/editar"
-            className="flex justify-between items-center w-full text-left font-light bg-highlighted text-white mt-7 p-3"
-          >
-            Editar atividade
-            <ArrowRight strokeWidth={1} />
-          </Link>
+    <Main>
+      {/* Task info */}
+      <div className=" space-y-10">
+        <div className="pt-7">
+          <h1 className="text-center font-medium text-xl">
+            Pitch de apresentação
+          </h1>
+          <span className="block text-center">19/05/2023</span>
         </div>
-      </Delay>
-    </main>
+
+        <hr className="border-[#C6C6C6]" />
+
+        {/* Task details */}
+        <div>
+          <h2 className="font-medium">Detalhes da tarefa:</h2>
+          <p className="p-3 text-justify text-xs leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            iure corporis hic sit voluptate odit consequatur, eaque eum
+            voluptatum, quaerat dolorum veritatis quis exercitationem error
+            nesciunt quibusdam repudiandae. Assumenda, placeat!
+          </p>
+        </div>
+
+        {/* Attached files */}
+        <div>
+          <h2 className="font-medium mb-4">Materiais disponíveis:</h2>
+          <div className="border-2 border-highlighted text-highlighted p-3 flex gap-2 underline">
+            <Image alt="image icon" />
+            Arquivo1.png
+          </div>
+        </div>
+
+        {/* Edit task link */}
+        <Link
+          href="pitch/editar"
+          className="flex justify-between items-center w-full text-left font-light bg-highlighted text-white mt-7 p-3"
+        >
+          Editar atividade
+          <ArrowRight strokeWidth={1} />
+        </Link>
+      </div>
+
+      <H2 title="Entregues" />
+    </Main>
   )
 }
