aa3561b715ff72b4aa1ccd3a0c649e8df528cffb
Gabriel Mendes <gabriel.26056@gmail.com>
2023-08-30 22:40:29 -0300

    improve page layout

--- front/src/app/(authenticated)/coordenador-ETEC/turma/turma1/aluno/page.jsx
+++ front/src/app/(authenticated)/coordenador-ETEC/turma/turma1/aluno/page.jsx
@@ -27,54 +27,69 @@ export default function Perfil() {
 
       <hr className="border-[#C6C6C6] mt-8 mb-6" />
 
-      <div className="progress-info">
-        {/* Progress info */}
-        <h2 className="text-base lg:text-2xl mt-5 mb-8">
-          Atividades do programa P-TECH
-        </h2>
+      <div className="md:flex md:items-center md:justify-around md:gap-10">
+        {/* Left (PC) */}
+        <div>
+          <div className="progress-info">
+            {/* Progress info */}
+            <h2 className="text-base lg:text-2xl mt-5 mb-8">
+              Atividades do programa P-TECH
+            </h2>
+            <div className="flex items-center justify-between md:gap-10 my-4 text-right">
+              <CircleProgress ABSNumber={22} reference={10} unity="horas" />
+              <ul className="space-y-4 lg:text-center">
+                <li className="flex items-center gap-3 text-xxs lg:text-xs">
+                  <Check color="#0F62FE" />
+                  10 Horas concluídas
+                </li>
+                <li className="flex items-center gap-3 text-xxs lg:text-xs">
+                  <Clock4 color="#0F62FE" />
+                  12 horas restantes
+                </li>
+                <li className="flex items-center gap-3 text-xxs lg:text-xs">
+                  <Percent color="#0F62FE" />
+                  45% do caminho
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-        <div className="flex items-center justify-between md:gap-10 my-4 text-right">
-          <CircleProgress ABSNumber={22} reference={10} unity="horas" />
-          <ul className="space-y-4 lg:text-center">
-            <li className="flex items-center gap-3 text-xxs lg:text-xs">
-              <Check color="#0F62FE" />
-              10 Horas concluídas
-            </li>
-            <li className="flex items-center gap-3 text-xxs lg:text-xs">
-              <Clock4 color="#0F62FE" />
-              12 horas restantes
-            </li>
-            <li className="flex items-center gap-3 text-xxs lg:text-xs">
-              <Percent color="#0F62FE" />
-              45% do caminho
-            </li>
-          </ul>
+        <hr className="border-[#C6C6C6] mt-8 mb-6 md:invisible" />
+
+        {/* Right (PC) */}
+        {/* Options */}
+        <div className="grow">
+          <button className="flex justify-between items-center w-full text-left font-light bg-highlighted hover:brightness-110 transition-all text-white mt-7 p-3">
+            Gerar relatório
+            <FileText strokeWidth={1} />
+          </button>
+          {/* Mobile button */}
+          <Link
+            className="flex justify-between items-center md:w-0 w-full text-left font-light bg-highlighted hover:brightness-110 transition-all text-white my-3 p-3 md:absolute md:invisible"
+            href="aluno/editar"
+          >
+            Editar aluno
+            <Pen strokeWidth={1} />
+          </Link>
+          {/* PC button */}
+          <button
+            onClick={handleOpen}
+            className="absolute invisible w-0 md:static md:visible flex justify-between items-center md:w-full text-left font-light bg-highlighted hover:cursor-pointer hover:brightness-110 transition-all text-white p-3 my-3"
+          >
+            Editar aluno
+            <Pen strokeWidth={1} />
+          </button>
+
+          <button
+            // onClick={handleOpen}
+            className="flex justify-between items-center w-full text-left font-light bg-[#BE2528] hover:cursor-pointer hover:brightness-110 transition-all text-white mt-3 p-3"
+          >
+            Excluir aluno
+            <Trash2 strokeWidth={1} />
+          </button>
         </div>
       </div>
-
-      <hr className="border-[#C6C6C6] mt-8 mb-6" />
-
-      {/* Options */}
-      <button className="flex justify-between items-center w-full text-left font-light bg-highlighted hover:brightness-110 transition-all text-white mt-7 p-3">
-        Gerar relatório
-        <FileText strokeWidth={1} />
-      </button>
-
-      <Link
-        className="flex justify-between items-center w-full text-left font-light bg-highlighted hover:brightness-110 transition-all text-white my-3 p-3 md:absolute md:invisible"
-        href="aluno/editar"
-      >
-        Editar aluno
-        <Pen strokeWidth={1} />
-      </Link>
-
-      <button
-        onClick={handleOpen}
-        className="absolute invisible md:static md:visible flex justify-between items-center w-full text-left font-light bg-highlighted hover:cursor-pointer hover:brightness-110 transition-all text-white p-3 my-3"
-      >
-        Editar aluno
-        <Pen strokeWidth={1} />
-      </button>
 
       {/* Modal */}
       <Modal isOpen={isOpen} handleClose={handleClose} onlyDesktop>
@@ -111,14 +126,6 @@ export default function Perfil() {
           <FormSubmitButton title="Editar aluno" />
         </form>
       </Modal>
-
-      <button
-        // onClick={handleOpen}
-        className="flex justify-between items-center w-full text-left font-light bg-[#BE2528] hover:cursor-pointer hover:brightness-110 transition-all text-white mt-3 p-3"
-      >
-        Excluir aluno
-        <Trash2 strokeWidth={1} />
-      </button>
 
       {/* <Modal isOpen={isOpen} handleClose={handleClose}>
         <span className="md:text-2xl md:block">
