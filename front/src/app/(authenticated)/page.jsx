bde9c02785491f78831235ededa94198876a6224
Gabriel Mendes <gabriel.26056@gmail.com>
2023-09-02 17:16:28 -0300

    fix middleware problems

--- front/src/app/(authenticated)/page.jsx
+++ front/src/app/(authenticated)/page.jsx
@@ -1,15 +1,13 @@
 'use client'
-import { useRouter } from 'next/navigation'
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import ETECCoordinatorHome from '@/components/coordenador-ETEC/ETECCoordinatorHome'
 import IBMCoordinatorHome from '@/components/coordenador-IBM/IBMCoordinatorHome'
 import StudentHome from '@/components/aluno/StudentHome'
 
-import { PageContext } from '../../context/PageContext'
+import { PageContext } from '../context/PageContext'
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => router.push('/dashboard'), [router])
+  // useEffect(() => router.push('/dashboard'), [router])
   const { userType } = useContext(PageContext)
 
   return userType === 'aluno' ? (
--- front/src/middleware.js
+++ front/src/middleware.js
@@ -4,23 +4,21 @@
 import { NextResponse } from 'next/server'
 
 const loginUrl = `${process.env.NEXT_PUBLIC_WEB_URL}login`
-// const homeUrl = `${process.env.NEXT_PUBLIC_WEB_URL}dashboard`
 
 export function middleware(req) {
-  if (!req.url.includes('login')) {
-    // Auth Token
-    // const token = req.cookies.get('token')?.value
-    const token = 'asd'
-    // Se o token não existir, o usuário não está logado, logo, ele será redirecionado para a tela de login
-    if (!token) {
-      return NextResponse.redirect(loginUrl, {
-        headers: {
-          'Set-Cookie': `redirectTo=${req.url}; Path=/; HttpOnly; max-age=20;`,
-        },
-      })
-    }
-    return NextResponse.next()
+  console.log('Teste')
+  // Auth Token
+  const token = req.cookies.get('token')?.value
+
+  // Se o token não existir, o usuário não está logado, logo, ele será redirecionado para a tela de login
+  if (!token) {
+    return NextResponse.redirect(loginUrl, {
+      headers: {
+        'Set-Cookie': `redirectTo=${req.url}; Path=/; HttpOnly; max-age=20;`,
+      },
+    })
   }
+  return NextResponse.next()
 }
 
 export const config = {
@@ -28,8 +26,6 @@ export const config = {
     '/aluno/:path*',
     '/coordenador-ETEC/:path*',
     '/coordenador-IBM/:path*',
-    // '/((?!login).*)',
-    '/dashboard/:path*',
-    '/(page.jsx.*)',
+    '/',
   ],
 }
