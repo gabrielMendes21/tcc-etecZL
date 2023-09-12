7dcb635a6e2ffc50c2dcc7c7b2b150f1a9d228af
Gabriel Mendes <gabriel.26056@gmail.com>
2023-09-12 07:49:31 -0300

    fetch student stats

--- front/src/components/aluno/StudentHome.jsx
+++ front/src/components/aluno/StudentHome.jsx
@@ -1,10 +1,6 @@
 import Main from '../Main'
 import UserInfo from '../UserInfo'
-import Link from 'next/link'
-import Task from '../Task'
-import TradLink from '../Link'
 import CircleProgress from '../circle/CircleProgress'
-import { Check, Clock4, Percent } from 'lucide-react'
 import PendingTasks from '../PendingTasks'
 
 export default async function StudentHome() {
@@ -26,17 +22,6 @@ export default async function StudentHome() {
 
           <div className="flex items-center justify-start gap-10 my-4">
             <CircleProgress unity="horas" />
-            <ul className="space-y-4 lg:text-center">
-              <li className="flex items-center text-left gap-3 text-xxs lg:text-xs">
-                <Check color="#0F62FE" />x Horas concluídas
-              </li>
-              <li className="flex items-center gap-3 text-xxs lg:text-xs">
-                <Clock4 color="#0F62FE" />x horas restantes
-              </li>
-              <li className="flex items-center gap-3 text-xxs lg:text-xs">
-                <Percent color="#0F62FE" />x do caminho
-              </li>
-            </ul>
           </div>
         </div>
       </div>
--- front/src/components/circle/CircleProgress.jsx
+++ front/src/components/circle/CircleProgress.jsx
@@ -3,6 +3,7 @@
 import { useContext } from 'react'
 import './styles.css'
 import { PageContext } from '@/app/context/PageContext'
+import { Check, Clock4, Percent } from 'lucide-react'
 
 export default function CircleProgress({ unity }) {
   const { user } = useContext(PageContext)
@@ -15,51 +16,67 @@ export default function CircleProgress({ unity }) {
   const dashOffset = dashArray - (dashArray * percentage) / 100
 
   return (
-    <div>
-      <svg
-        width={circleSize}
-        height={circleSize}
-        viewBox={`0 0 ${circleSize} ${circleSize}`}
-      >
-        <circle
-          className="circle-background fill-none stroke-[#DDD]"
-          cx={circleSize / 2}
-          cy={circleSize / 2}
-          strokeWidth="15px"
-          r={radius}
-        />
+    <div className="flex items-center justify-start gap-10 my-4">
+      <div>
+        <svg
+          width={circleSize}
+          height={circleSize}
+          viewBox={`0 0 ${circleSize} ${circleSize}`}
+        >
+          <circle
+            className="circle-background fill-none stroke-[#DDD]"
+            cx={circleSize / 2}
+            cy={circleSize / 2}
+            strokeWidth="15px"
+            r={radius}
+          />
+          <circle
+            className="circle-progress fill-none stroke-highlighted "
+            cx={circleSize / 2}
+            cy={circleSize / 2}
+            strokeWidth="15px"
+            r={radius}
+            style={{
+              strokeDasharray: dashArray,
+              strokeDashoffset: dashOffset,
+            }}
+            transform={`rotate(-90 ${circleSize / 2} ${circleSize / 2})`}
+          />
+          <text
+            x="53%"
+            y="48%"
+            dy="0.3em"
+            textAnchor="middle"
+            className="text-3xl font-semibold"
+          >
+            {percentage}%
+          </text>
+          <text
+            x="50%"
+            y="65%"
+            dy="0.3em"
+            textAnchor="middle"
+            className="text-xs"
+          >
+            {hoursCompleted}/{annualHours} {unity}
+          </text>
+        </svg>
+      </div>
 
-        <circle
-          className="circle-progress fill-none stroke-highlighted "
-          cx={circleSize / 2}
-          cy={circleSize / 2}
-          strokeWidth="15px"
-          r={radius}
-          style={{
-            strokeDasharray: dashArray,
-            strokeDashoffset: dashOffset,
-          }}
-          transform={`rotate(-90 ${circleSize / 2} ${circleSize / 2})`}
-        />
-        <text
-          x="53%"
-          y="48%"
-          dy="0.3em"
-          textAnchor="middle"
-          className="text-3xl font-semibold"
-        >
-          {percentage}%
-        </text>
-        <text
-          x="50%"
-          y="65%"
-          dy="0.3em"
-          textAnchor="middle"
-          className="text-xs"
-        >
-          {hoursCompleted}/{annualHours} {unity}
-        </text>
-      </svg>
+      <ul className="space-y-4 lg:text-center">
+        <li className="flex items-center text-left gap-3 text-xxs lg:text-xs">
+          <Check color="#0F62FE" />
+          {hoursCompleted} Horas concluídas
+        </li>
+        <li className="flex items-center gap-3 text-left text-xxs lg:text-xs">
+          <Clock4 color="#0F62FE" />
+          {annualHours - hoursCompleted} horas restantes
+        </li>
+        <li className="flex items-center gap-3 text-left text-xxs lg:text-xs">
+          <Percent color="#0F62FE" />
+          {Math.round((hoursCompleted / annualHours) * 100)} do caminho
+        </li>
+      </ul>
     </div>
   )
 }
