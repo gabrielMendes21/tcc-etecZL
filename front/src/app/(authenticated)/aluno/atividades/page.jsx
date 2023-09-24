8a1d9e5d4e427ad803f2aa4091c2cf4873862440
Gabriel Mendes <gabriel.26056@gmail.com>
2023-09-24 13:26:17 -0300

    fetch all student tasks

--- front/src/app/(authenticated)/aluno/atividades/page.jsx
+++ front/src/app/(authenticated)/aluno/atividades/page.jsx
@@ -1,63 +1,47 @@
-'use client'
-
 import H1 from '@/components/H1'
 import Main from '@/components/Main'
-import { useState } from 'react'
-import { Box, Tab } from '@mui/material'
-import { TabContext, TabList, TabPanel } from '@mui/lab'
-import Link from 'next/link'
-import Task from '@/components/Task'
+import axios from 'axios'
+import { cookies } from 'next/headers'
 
-export default function Atividades() {
-  const [value, setValue] = useState('1')
-  const handleChange = (event, newValue) => setValue(newValue)
+export default async function Atividades() {
+  // Get the token
+  const cookieStore = cookies()
+  const token = cookieStore.get('auth-token').value
+
+  // Get the data from the token
+  const userResponse = await axios(
+    `${process.env.NEXT_PUBLIC_WEB_URL}api/login`,
+    {
+      params: {
+        token,
+      },
+    },
+  )
+
+  // Get all tasks
+  const tasksResponse = await axios(
+    `${process.env.NEXT_PUBLIC_WEB_URL}api/aluno/atividades?id=${userResponse.data.sub}`,
+  )
+
+  const tasks = tasksResponse.data
+
+  // Pending Tasks
+  const pendingTasks = tasks.filter((task) => task.entregue === false)
+
+  // Sent Tasks
+  const sentTasks = tasks.filter((task) => task.entregue)
+
+  // Overdue Tasks
+  const now = new Date()
+  now.setHours(now.getHours() - 3)
+
+  const overdueTasks = tasks.filter(
+    (task) => new Date(task.atividade.prazoEntrega) < now,
+  )
 
   return (
     <Main>
       <H1 title="Atividades" />
-
-      <Box>
-        <TabContext value={value}>
-          <Box
-            sx={{
-              borderBottom: 1,
-              borderColor: 'divider',
-              width: {
-                xs: '99%', // 0px => 599px
-                sm: '100%', // 600px => 899px
-                xl: '100%', // 1536px
-              },
-            }}
-          >
-            <TabList
-              aria-label="Activities tabs"
-              variant="scrollable"
-              scrollButtons="auto"
-              onChange={handleChange}
-            >
-              <Tab label="Pendentes" value="1" />
-              <Tab label="Entregues" value="2" />
-              <Tab label="Atrasadas" value="3" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-            <Link href="atividades/pre-projeto" className="block mb-5">
-              <Task name="Pre-projeto" hours={2} dueDate="19/05/2023" />
-            </Link>
-            <Task name="Skills Build" hours={30} dueDate="25/06/2023" />
-          </TabPanel>
-          <TabPanel value="2">
-            <Task name="Pitch de apresentação" hours={1} dueDate="19/05/2023" />
-            <span className="block my-5"></span>
-            <Task name="Cursos DIO" hours={1} dueDate="05/12/2023" />
-          </TabPanel>
-          <TabPanel value="3">
-            <Task name="Webinar" hours={20} dueDate="20/20/2023" />
-            <span className="block my-5"></span>
-            <Task name="Palestra" hours={0.5} dueDate="16/11/2023" />
-          </TabPanel>
-        </TabContext>
-      </Box>
     </Main>
   )
 }
