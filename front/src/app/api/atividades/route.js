f6264d9134f857294fd4a87b0d3f9f21a306a398
Gabriel Mendes <gabriel.26056@gmail.com>
2023-09-25 21:56:32 -0300

    add axios instance using

--- front/src/app/api/atividades/route.js
+++ front/src/app/api/atividades/route.js
@@ -2,7 +2,7 @@ import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const atividades = await prisma.alunoAtividade.findMany()
+  const atividades = await prisma.entrega.findMany()
 
   return NextResponse.json(atividades)
 }
