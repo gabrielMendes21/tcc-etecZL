4e75c7c61fc0716a3078c7cd59a58727962af964
Gabriel Mendes <gabriel.mendes88@etec.sp.gov.br>
2023-10-04 14:32:08 -0300

    add activity class

---
+++ front/src/classes/Atividade.js
@@ -0,0 +1,95 @@
+import { TurnRightSharp } from "@mui/icons-material"
+
+/* eslint-disable accessor-pairs */
+export default class Atividade {
+    constructor() {
+        this.codAtividade = 0
+        this.titulo = ""
+        this.descricao = ""
+        this.tipoAtividade = ""
+        this.anexos = ""
+        this.horasAtividade = 0
+        this.dataCriacao = ""
+        this.prazoEntrega = ""
+    }
+
+    set setCodAtividade(newValue) {
+        this.codAtividade = newValue
+    }
+
+    get getCodAtividade() {
+        return this.codAtividade
+    }
+
+    set setTitulo(newValue) {
+        this.titulo = newValue
+    }
+
+    get getTitulo() {
+        return this.titulo
+    }
+
+    set setDescricao(newValue) {
+        this.descricao = newValue
+    }
+
+    get getDescricao() {
+        return this.descricao
+    }
+
+    set setTipoAtividade(newValue) {
+        this.tipoAtividade = newValue
+    }
+
+    get getTipoAtividade() {
+        return this.tipoAtividade
+    }
+
+    set setAnexos(newValue) {
+        this.anexos =  newValue
+    }
+
+    get getAnexos() {
+        return this.anexos
+    }
+
+    set setHorasAtividade(newValue) {
+        this.horasAtividade = newValue
+    }
+
+    get getHorasAtividade() {
+        return this.horasAtividade
+    }
+
+    set setDataCriacao(newValue) {
+        this.dataCriacao = newValue
+    }
+
+    get getDataCriacao() {
+        return this.dataCriacao
+    }
+
+    set setPrazoEntrega(newValue) {
+        this.prazoEntrega = newValue
+    }
+
+    get getPrazoEntrega() {
+        return this.prazoEntrega
+    }
+
+    setAtividade(atividade) {
+        return true
+    }
+
+    getAtividade() {
+        return {...this}
+    }
+
+    updateAtividade(atividade) {
+        return true
+    }
+
+    deleteAtividade() {
+        return true
+    }
+}
