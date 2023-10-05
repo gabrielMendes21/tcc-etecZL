184462ca9282ad7f448352175962791237521915
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-04 21:22:45 -0300

    fix userInfo component

--- front/src/components/UserInfo.jsx
+++ front/src/components/UserInfo.jsx
@@ -7,7 +7,9 @@ export default function UserInfo() {
   const { user } = useContext(PageContext)
   const firstName = user?.nome?.split(' ')[0]
 
-  const coordinatorType = user?.tipoCoordenador
+  const coordinatorType =
+    (user?.tipoUsuario === 'Coordenador ETEC' && user?.tipoUsuario) ||
+    (user?.tipoUsuario === 'Coordenador IBM' && user?.tipoUsuario)
 
   return (
     <>
@@ -15,9 +17,9 @@ export default function UserInfo() {
         <h1 className="text-xl md:text-3xl md:my-5 font-medium py-7">
           Olá, {firstName}!
         </h1>
-        <span className="text-xs md:text-xl">{`${
-          coordinatorType || `${user?.turma}, ${user?.escola?.nomeEscola}`
-        } `}</span>
+        <span className="text-xs md:text-xl">
+          {coordinatorType || `${user?.turma}, ${user?.escola?.nomeEscola}`}
+        </span>
       </div>
 
       <hr className="border-[#C6C6C6] md:my-2" />
