d7dd73a34652c453550417f98c3885900c25b6a2
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-16 21:44:53 -0300

    add support request answer to api response

--- front/src/app/api/solicitacoesSuporte/route.js
+++ front/src/app/api/solicitacoesSuporte/route.js
@@ -2,7 +2,11 @@ import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const supportRequests = await prisma.solicitacaoSuporte.findMany()
+  const supportRequests = await prisma.solicitacaoSuporte.findMany({
+    include: {
+      Resposta: true,
+    },
+  })
 
   return NextResponse.json(supportRequests)
 }
