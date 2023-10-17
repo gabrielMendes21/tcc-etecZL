9e6d08806edb8407a1f09b5840af52f2cdc761e5
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-16 21:36:36 -0300

    collect dynamic data of support requests for IBM coordinator

--- front/src/app/(authenticated)/coordenador-IBM/suporte/page.jsx
+++ front/src/app/(authenticated)/coordenador-IBM/suporte/page.jsx
@@ -1,52 +1,23 @@
-import Card from '@/components/Card'
+import AnsweredSupportRequests from '@/components/AnsweredSupportRequests'
 import H1 from '@/components/H1'
 import H2 from '@/components/H2'
 import Main from '@/components/Main'
-import Link from 'next/link'
+import PendingSupportRequests from '@/components/PendingSupportRequests'
 
 export default function Suporte() {
   return (
     <Main>
-      {/* Support requests */}
       <H1 title="Solicitações de suporte" />
 
+      {/* Answered requests */}
       <H2 title="Respondidas" />
-      <div className="classes space-y-2">
-        <Link href="suporte/suporte2">
-          <Card>
-            <span className="text-base">Henrique Machado</span>
-            <span>3º AMS, ETEC Zona Leste - 12/05/2023</span>
-          </Card>
-        </Link>
-        <Card>
-          <span className="text-base">Gabriel da Silva Mendes</span>
-          <span>3º AMS, ETEC Zona Leste - 08/05/2023</span>
-        </Card>
-        <Card>
-          <span className="text-base">Andrei Luiz Florencio Matias</span>
-          <span>3º AMS, ETEC Zona Leste - 09/05/2023</span>
-        </Card>
-      </div>
+      <AnsweredSupportRequests />
 
       <hr className="border-[#C6C6C6] mt-7" />
 
+      {/* Not-answered requests */}
       <H2 title="Não respondidas" />
-      <div className="classes space-y-2">
-        <Link href="suporte/suporte1">
-          <Card>
-            <span className="text-base">Danilo Costa Rodrigues</span>
-            <span>Reconsiderações - 12/05/2023</span>
-          </Card>
-        </Link>
-        <Card>
-          <span className="text-base">João Vitor</span>
-          <span>Horas não contabilizadas - 08/05/2023</span>
-        </Card>
-        <Card>
-          <span className="text-base">Paulo Enrick</span>
-          <span>Problemas no site - 09/05/2023</span>
-        </Card>
-      </div>
+      <PendingSupportRequests />
     </Main>
   )
 }
