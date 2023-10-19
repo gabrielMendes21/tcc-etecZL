73cde6b29636dd1f2476ef6f9d65602aba2b997a
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-19 16:51:53 -0300

    add create support request route for mobile

--- front/src/app/(authenticated)/aluno/suporte/nova-solicitacao/page.jsx
+++ front/src/app/(authenticated)/aluno/suporte/nova-solicitacao/page.jsx
@@ -1,14 +1,35 @@
+'use client'
+
+import { PageContext } from '@/app/context/PageContext'
 import FormSubmitButton from '@/components/FormSubmitButton'
 import H1 from '@/components/H1'
 import Main from '@/components/Main'
+import { api } from '@/lib/api'
+import { useRouter } from 'next/navigation'
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function NovaSolicitação() {
+  const router = useRouter()
+
+  const user = useContext(PageContext)
+
+  // Form control
+  const { register, handleSubmit } = useForm()
+  const onSubmit = async (data) => {
+    const response = await api.post(`/solicitacaoSuporte?studentId=${user?.user?.sub}`, {
+      ...data,
+    })
+
+    router.push('/aluno/suporte/')
+  }
+
   return (
     <Main>
       <H1 title="Solicitar suporte" />
 
       {/* Form */}
-      <form action="" className="pt-5">
+      <form action="" method='POST' onSubmit={handleSubmit(onSubmit)} className="pt-5">
         <label htmlFor="subject" className="text-sm">
           Assunto
         </label>
@@ -17,6 +38,8 @@ export default function NovaSolicitação() {
           id="subject"
           placeholder="Digite o assunto da sua solicitação"
           className="mt-3 mb-8 border-b block w-full border-black bg-[#F4F4F4] focus:outline-highlighted rounded-none p-2"
+          required
+          {...register("subject")}
         />
 
         <label htmlFor="message" className="text-sm">
@@ -28,6 +51,8 @@ export default function NovaSolicitação() {
           rows="10"
           className="mt-3 mb-6 w-full resize-none border-b block border-black bg-[#F4F4F4] focus:outline-highlighted p-2"
           placeholder="Digite sua mensagem"
+          required
+          {...register("message")}
         ></textarea>
 
         <hr className="border[#C6C6C6]" />
