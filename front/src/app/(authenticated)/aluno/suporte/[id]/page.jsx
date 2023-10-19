c476fb201e6586615ad4c8deed994d4dbb3244d7
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-19 09:58:59 -0300

    collect dynamic data for student support requests

---
+++ front/src/app/(authenticated)/aluno/suporte/[id]/page.jsx
@@ -0,0 +1,105 @@
+import Main from '@/components/Main'
+import { api } from '@/lib/api'
+
+export async function generateStaticParams() {
+  const supportRequestsResponse = await api.get('/solicitacoesSuporte')
+  const supportRequests = supportRequestsResponse.data
+
+  return supportRequests.map((supportRequest) => {
+    return {
+      id: supportRequest.id.toString(),
+    }
+  })
+}
+export default async function Suporte({ params }) {
+  const convertISOtoDateFormat = (string) => {
+    let date = new Date(string)
+    date = date.toLocaleDateString()
+
+    return date
+  }
+
+  const supportRequestresponse = await api.get('/solicitacaoSuporte', {
+    params: {
+      id: Number(params.id),
+    },
+  })
+
+  const supportRequest = supportRequestresponse.data
+
+  return (
+    <Main>
+      {supportRequest.Resposta ? (
+        <>
+          <h1 className="block mt-7 font-medium text-xl text-center">
+            Suporte
+          </h1>
+          {/* Request subject */}
+          <span className="block font-light text-center">
+            {supportRequest.titulo}
+          </span>
+          <hr className="border-[#C6C6C6] my-8" />
+          {/* Support message */}
+          <div className="my-8">
+            <h2 className="font-medium md:text-xl">Mensagem:</h2>
+            <p className="py-3 text-justify text-xs md:text-base leading-relaxed">
+              {supportRequest.conteudo}
+            </p>
+
+            {/* Support message onwer */}
+            <span className="font-light text-xs text[#525252]">
+              {convertISOtoDateFormat(supportRequest.dataCriacao)}
+            </span>
+          </div>
+
+          <hr className="border-[#C6C6C6] my-8" />
+
+          {/* Support response */}
+          <div className="my-8">
+            <h2 className="font-medium md:text-xl">Resposta:</h2>
+            <p className="py-3 text-justify text-xs md:text-base leading-relaxed">
+              {supportRequest.Resposta.resposta}
+            </p>
+
+            <span className="text-xs block">
+              {supportRequest.Resposta.coordenador.nome} -{' '}
+              {supportRequest.Resposta.coordenador.tipoUsuario.tipoUsuario}
+            </span>
+            <span className="font-light text-xs text[#525252]">
+              {convertISOtoDateFormat(supportRequest.Resposta.dataResposta)}
+            </span>
+          </div>
+          <div className="flex justify-between items-center w-full text-left font-light bg-[#b8b8b8] text-white mt-7 p-3">
+            Respondida!
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="block mt-7 font-medium text-xl text-center">
+            Suporte
+          </h1>
+          {/* Request subject */}
+          <span className="block font-light text-center">
+            {supportRequest.titulo}
+          </span>
+
+          <hr className="border-[#C6C6C6] my-8" />
+
+          {/* Support message */}
+          <div className="my-8">
+            <h2 className="font-medium md:text-xl">Mensagem:</h2>
+            <p className="py-3 text-justify text-xs md:text-base leading-relaxed">
+              {supportRequest.conteudo}
+            </p>
+            {/* Support request author */}
+            <span className="font-light text-xs text[#525252]">
+              {convertISOtoDateFormat(supportRequest.dataCriacao)}
+            </span>
+          </div>
+
+          <hr className="border-[#C6C6C6] my-8" />
+        </>
+      )}
+    </Main>
+  )
+}
--- front/src/app/(authenticated)/aluno/suporte/page.jsx
+++ front/src/app/(authenticated)/aluno/suporte/page.jsx
@@ -4,8 +4,10 @@ import H1 from '@/components/H1'
 import Main from '@/components/Main'
 import NewButton from '@/components/NewButton'
 import NewSupportRequestModal from '@/components/newSupportRequestModal'
+import { api } from '@/lib/api'
 import { auth } from '@/lib/auth'
 import { cookies } from 'next/headers'
+import Link from 'next/link'
 
 export default async function Suporte() {
   // Get the token
@@ -13,32 +15,70 @@ export default async function Suporte() {
   const token = cookieStore.get('auth-token').value
 
   const student = await auth(token)
+
+  const responses = await Promise.all([
+    api.get('/aluno/solicitacoesSuporte/respondidas', {
+      params: {
+        student: student.sub,
+      },
+    }),
+    api.get('/aluno/solicitacoesSuporte/enviadas', {
+      params: {
+        student: student.sub,
+      },
+    }),
+  ])
+
+  const pendingSupportRequests = responses[1].data
+
+  const answeredSupportRequests = responses[0].data
+
   return (
     <Main>
       <H1 title="Suporte" />
 
       {/* Complaints sent */}
       <Dropdown title="Solicitações enviadas">
-        <Card>
-          <span className="text-base">Horas não contabilizadas</span>
-          <span>Aguardando resposta...</span>
-        </Card>
-        <Card>
-          <span className="text-base">Erro na entrega</span>
-          <span>Aguardando resposta...</span>
-        </Card>
+        {pendingSupportRequests.length > 0 ? (
+          pendingSupportRequests.map((supportRequest) => {
+            return (
+              <Link href={`suporte/${supportRequest.id}`}>
+                <Card>
+                  <span className="text-base">{supportRequest.titulo}</span>
+                  <span>Aguardando resposta...</span>
+                </Card>
+              </Link>
+            )
+          })
+        ) : (
+          <span className="text-black/30 block">
+            Você não possui nenhuma solicitação pendente
+          </span>
+        )}
       </Dropdown>
 
       {/* Complaints answered  */}
       <Dropdown title="Solicitações respondidas">
-        <Card>
-          <span className="text-base">Atraso nas atividades</span>
-          <span>Respondida em 20/09/2023</span>
-        </Card>
-        <Card>
-          <span className="text-base">Dúvidas sobre a atividade</span>
-          <span>Respondida em 12/05/2023</span>
-        </Card>
+        {answeredSupportRequests.length > 0 ? (
+          answeredSupportRequests.map((supportRequest) => {
+            const answerDate = new Date(supportRequest.Resposta.dataResposta)
+              .toLocaleString('pt-BR')
+              .split(', ')[0]
+
+            return (
+              <Link href={`suporte/${supportRequest.id}`}>
+                <Card>
+                  <span className="text-base">{supportRequest.titulo}</span>
+                  <span>Respondida em {answerDate}</span>
+                </Card>
+              </Link>
+            )
+          })
+        ) : (
+          <span className="text-black/30 block">
+            Você não possui nenhuma solicitação respondida
+          </span>
+        )}
       </Dropdown>
 
       {/* New support request button */}
