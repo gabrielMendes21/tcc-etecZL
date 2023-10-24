122b6fe5cd5ff0abdbb74096247332da350c2dbc
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-24 08:49:08 -0300

    add task attachment

--- front/prisma/seed.js
+++ front/prisma/seed.js
@@ -90,6 +90,7 @@ async function seed() {
         horasAtividade: 2,
         prazoEntrega: '2023-11-10T10:00:00.000Z',
         codTipoAtividade: 1,
+        anexos: 'jsoqoancojjgo1-exemplo-planilha.png',
       },
       {
         titulo: 'Horas flexíveis',
