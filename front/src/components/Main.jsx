60749ce408cc227a12c5f4ba02f2a6b20007b2a1
Gabriel Mendes <gabriel.26056@gmail.com>
2023-10-26 17:25:37 -0300

    increase horizontal padding

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -2,7 +2,7 @@ import Delay from './LoadDelay'
 
 export default function Main({ children, className }) {
   return (
-    <main className={`px-4 my-16 md:px-36 md:my-20 ${className}`}>
+    <main className={`px-4 my-16 md:px-80 md:my-20 ${className}`}>
       <Delay>{children}</Delay>
     </main>
   )
