65ac3d3eb0944af046eb9f8cb5350ae251cf3f27
Gabriel Mendes <gabriel.26056@gmail.com>
2023-11-05 13:06:49 -0300

    fetch only classes of the current year

--- front/src/app/api/turmas/route.js
+++ front/src/app/api/turmas/route.js
@@ -2,7 +2,11 @@ import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const classes = await prisma.turma.findMany()
+  const classes = await prisma.turma.findMany({
+    where: {
+      ano: new Date().getFullYear(),
+    },
+  })
 
   return NextResponse.json(classes)
 }
