313a7b79aba6423a3b3170c911cb7673328629bf
daniel <danielcarcosta@hotmail.com>
2023-11-09 14:19:18 -0300

    fix generate report error

--- front/src/app/(authenticated)/coordenador-IBM/turma/[id]/relatorio/page.jsx
+++ front/src/app/(authenticated)/coordenador-IBM/turma/[id]/relatorio/page.jsx
@@ -1,16 +1,13 @@
 'use client'
 
-import Main from '@/components/Main'
 import Report from '@/components/Report'
 import StudentInfo from '@/components/StudentInfo'
 import { api } from '@/lib/api'
-import { FileBarChart } from 'lucide-react'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function Relatorio() {
-  const [classData, setClassData] = useState({})
+  const [students, setStudents] = useState()
 
   const pathname = usePathname()
   const segments = pathname.split('/')
@@ -25,8 +22,7 @@ export default function Relatorio() {
         },
       })
       .then((response) => {
-        setClassData(response.data)
-        console.log(response.data)
+        setStudents(response.data)
       })
       .catch((e) => {
         console.log(e)
@@ -34,33 +30,23 @@ export default function Relatorio() {
   }, [])
 
   return (
-    <Main>
-      <Link
-        href="#"
-        className="bg-red-700 inline-flex p-3 gap-3 items-center text-white hover:brightness-110 transition-all mt-8"
-      >
-        Gerar PDF
-        <FileBarChart size={20} />
-      </Link>
-      <div className="mt-8">
-        {classData.Usuario?.map((student) => {
-          return (
-            <>
-              <div className="flex justify-between">
-                {/* student info */}
-                <StudentInfo
-                  student={student}
-                  key={student.id}
-                  school={classData.escola.nomeEscola}
-                />
-                {/* Report */}
-                <Report student={student} key={student.id} />
-              </div>
-              <hr className="my-6" />
-            </>
-          )
-        })}
-      </div>
-    </Main>
+    <div className="mt-8">
+      {students?.map((student) => {
+        return (
+          <>
+            <div className="flex justify-between">
+              {/* student info */}
+              <StudentInfo
+                student={student}
+                school={student.turma.escola.nomeEscola}
+              />
+              {/* Report */}
+              <Report student={student} key={student.id} />
+            </div>
+            <hr className="my-6" />
+          </>
+        )
+      })}
+    </div>
   )
 }
